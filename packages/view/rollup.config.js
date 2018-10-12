50d48c82133eb1174dc150db67e40da3b9db3dae
orlo wang <ow.cc@outlook.com>
2018-10-13 02:04:01 +0800

    add banner for rollop

--- packages/view/rollup.config.js
+++ packages/view/rollup.config.js
@@ -1,16 +1,24 @@
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import VuePlugin from "rollup-plugin-vue";
+import pkg from "./package.json";
 
+const banner = `/*
+ \* library: ${pkg.name}
+ \* version: ${pkg.version}
+ \* author: ${pkg.author || "Orlo Wang <ow.cc#outlook.com>"}
+ */`;
 export default {
   input: "index.js",
   output: [
     {
+      banner,
       format: "cjs",
       name: "view",
       file: "lib/index.common.js"
     },
     {
+      banner,
       format: "esm",
       name: "view",
       file: "lib/index.esm.js"
