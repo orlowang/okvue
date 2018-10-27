306958d50eead25c470bed6adaa53beb437a70fc
orlo wang <ow.cc@outlook.com>
2018-10-27 20:31:32 +0800

    clear lib before build

--- packages/view/rollup.config.js
+++ packages/view/rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import VuePlugin from "rollup-plugin-vue";
+import cleaner from "rollup-plugin-cleaner";
 import pkg from "./package.json";
 
 const banner = `/*
@@ -25,6 +26,9 @@ export default {
     }
   ],
   plugins: [
+    cleaner({
+      targets: ["./lib/"]
+    }),
     VuePlugin(),
     resolve({
       browser: true
