629a74aebdfbdd97318df53589c97e22c3d2fa41
Orlo Wang <ow.cc@outlook.com>
2018-12-13 01:22:51 +0800

    clear

--- packages/cli/demo/src/service/storage.js
+++
@@ -1,5 +0,0 @@
-import Storage from "@okvue/storage";
-
-export default function storageService(Vue) {
-  Vue.prototype.$storage = new Storage();
-}
--- packages/cli/demo/src/views/element.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <iframe src="http://element-cn.eleme.io/#/zh-CN/component/button" frameborder="0" :style="{ width: '100%' }"/>
-</template>
